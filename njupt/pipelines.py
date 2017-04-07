73cbcd3f4d198286fb5a711780cb8b172343629b
CocoPrince <821414815@qq.com>
2017-04-07 13:15:09 +0800

    Update pipelines.py

--- njupt/pipelines.py
+++ njupt/pipelines.py
@@ -6,6 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+import codecs
 
 class NjuptPipeline(object):
     def __init__(self):
@@ -18,3 +19,34 @@ class NjuptPipeline(object):
         self.file.write(item['news_url'])
         self.file.write("\n")
         return item
+
+    from twisted.enterprise import adbapi
+import pymysql
+import pymysql.cursors
+class NjuptPipeline(object):
+    #数据库参数
+    def __init__(self):
+        dbargs = dict(
+             host = '127.0.0.1',
+             db = 'lirui',          #数据库
+             user = 'zhanglirui',    #用户名
+             passwd = 'zlr1998',    #密码
+             cursorclass = pymysql.cursors.DictCursor,
+             charset = 'utf8',
+            # use_unicode = True
+            )
+        self.dbpool = adbapi.ConnectionPool('pymysql',**dbargs)
+
+    '''
+    The default pipeline invoke function
+    '''
+    def process_item(self, item,spider):
+        res = self.dbpool.runInteraction(self.insert_into_table,item)
+        return item
+    #插入的表，此表需要事先建好
+    def insert_into_table(self,conn,item):
+            conn.execute('insert into my(title, date,link) values(%s,%s,%s)', (
+                item['title'][0],
+                item['date'][0],
+                item['link'][0])
+                )
